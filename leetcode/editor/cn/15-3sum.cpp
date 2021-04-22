5d836163bfcba9a40aba9893e800ed8f4f5092bf
mahone <731978588@qq.com>
2021-04-22 18:26:44 +0800

    Initial commit

---
+++ leetcode/editor/cn/15-3sum.cpp
@@ -0,0 +1,87 @@
+//给你一个包含 n 个整数的数组 nums，判断 nums 中是否存在三个元素 a，b，c ，使得 a + b + c = 0 ？请你找出所有和为 0 且不重
+//复的三元组。 
+//
+// 注意：答案中不可以包含重复的三元组。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：nums = [-1,0,1,2,-1,-4]
+//输出：[[-1,-1,2],[-1,0,1]]
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：nums = []
+//输出：[]
+// 
+//
+// 示例 3： 
+//
+// 
+//输入：nums = [0]
+//输出：[]
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 0 <= nums.length <= 3000 
+// -105 <= nums[i] <= 105 
+// 
+// Related Topics 数组 双指针 
+// 👍 3267 👎 0
+
+
+#include "include/headers.h"
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        vector<vector<int>> res;
+        if(nums.size()<3){return res;}
+        int l,r;
+        for(int i=0;i<nums.size()-2;i++){
+            if(nums[i]>0){
+                break;
+            }
+            if( i > 0 && nums[i] == nums[i-1]){
+                continue;
+            }
+            l=i+1;
+            r=nums.size()-1;
+            while( l < r ){
+                if( nums[i] + nums[l] + nums[r] < 0){
+                    l++;
+                }else if(nums[i] + nums[l] + nums[r] > 0){
+                    r--;
+                }else{
+                    res.emplace_back(vector<int>{nums[i],nums[l],nums[r]});
+                    l++;
+                    r--;
+                    while(l<r&&nums[l]==nums[l-1]) l++;
+                    while(l<r&&nums[r]==nums[r+1]) r--;
+                }
+            }
+        }
+
+
+        return res;
+
+    }
+};
+//leetcode submit region end(Prohibit modification and deletion)
+
+
+int main()
+{
+    Solution s;
+    
+}
---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <list>
+#include <vector>
+#include <cstring>
+
+
+using namespace std;
+
+
+int main() {
+
+
+
+
+    return 0;
+}
